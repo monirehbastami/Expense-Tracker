6833e03d493606f6643d8d1a7ccd62bbc6180d42
Monireh Bastami <monirehbastami@gmail.com>
2024-02-08 12:44:43 -0800

    insert filter in expense list

--- expense/form.py
+++ expense/form.py
@@ -16,3 +16,11 @@ class ExpenseCreateForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'})
         }
         
+
+class FilterForm(forms.Form):
+    date1 = forms.DateField(label='Start Date', widget=forms.DateInput(attrs={'type': 'date','class': 'w-42 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}), required=False)
+    date2 = forms.DateField(label='End Date', widget=forms.DateInput(attrs={'type': 'date','class': 'w-42 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}), required=False)
+    amount1 = forms.FloatField(label='Min Price',widget=forms.TextInput(attrs={'class': 'w-32 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}), required=False,min_value=0)
+    amount2 = forms.FloatField(label='max Price',widget=forms.TextInput(attrs={'class': 'w-32 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}), required=False,min_value=0)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='---', widget=forms.Select(attrs={'class': 'w-42 px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}), required=False)
+            
--- expense/views.py
+++ expense/views.py
@@ -1,9 +1,13 @@
+import datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView,CreateView,UpdateView
+from expense.form import FilterForm
 from expense.mixins import ExpenseListMixin, ExpenseCreateMixin, ExpenseUpdateMixin
+from django.utils.dateparse import parse_date
+
 
 decorators = [never_cache, login_required(login_url='users:home')]
 
@@ -13,16 +17,46 @@ class ExpenseListView(ExpenseListMixin,ListView):
     template_name = 'expense_list_view.html'
     
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        queryset = super().get_queryset()
+        date1 = self.request.POST.get('date1')
+        date2 = self.request.POST.get('date2')
+        amount1 = self.request.POST.get('amount1')
+        amount2 = self.request.POST.get('amount2')
+        category = self.request.POST.get('category')
+        filters = {'user': self.request.user}
+        if date1:
+            filters['date__gte'] = date1
+        if date2:
+            filters['date__lte'] = date2
+        if amount1:
+            filters['amount__gte'] = amount1
+        if amount2:
+            filters['amount__lte'] = amount2
+        if category:
+            filters['category'] = category
+        if filters:
+            return queryset.filter(**filters)
+        else:
+            return queryset.filter(user=self.request.user)
     
     def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        context ={
-            'expenses':self.get_queryset()
+        form = FilterForm()
+        context = {
+            'form': form,
+            'expenses': self.get_queryset()
         }
-        return self.render_to_response(context)
-
-
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = FilterForm(request.POST)
+        if form.is_valid():
+            self.object_list = self.get_queryset()
+        context = {
+            'form': form,
+            'expenses': self.object_list
+        }
+        return render(request, self.template_name, context) 
+    
 @method_decorator(decorators, name="dispatch")
 class ExpenseCreateView(ExpenseCreateMixin,CreateView):
     template_name = 'expense_form.html' 
