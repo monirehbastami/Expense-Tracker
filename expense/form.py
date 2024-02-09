b2a3bc60bdc173fc2682bf3e9092964346394bea
Monireh Bastami <monirehbastami@gmail.com>
2024-02-09 07:19:20 -0800

    fix date style

--- expense/form.py
+++ expense/form.py
@@ -4,6 +4,7 @@ from categories.models import Category
 
 class ExpenseCreateForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='---', widget=forms.Select(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}))
+    
     class Meta:
         model = Expense
         fields = ['title','amount','date','category']
@@ -12,7 +13,7 @@ class ExpenseCreateForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}),
             'amount': forms.NumberInput(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}),
-            'date': forms.DateInput(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}),
+            'date': forms.DateInput(attrs={'type': 'date','class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'}),
             'category': forms.Select(attrs={'class': 'w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2'})
         }
         
